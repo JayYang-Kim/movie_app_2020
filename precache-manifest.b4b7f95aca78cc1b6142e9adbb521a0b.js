b7f5aaa781056982d1f519c1c23ef82d6e5e2c29
KIMJINYANG <sunlover011@gmail.com>
2020-07-07 19:22:16 +0900

    Updates

---
+++ precache-manifest.b4b7f95aca78cc1b6142e9adbb521a0b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "752ca18223239206739ca77e152b4971",
+    "url": "/movie_app_2020/index.html"
+  },
+  {
+    "revision": "1639da50ef170a412dbd",
+    "url": "/movie_app_2020/static/css/main.c4d4891a.chunk.css"
+  },
+  {
+    "revision": "e31c14df905538679f9a",
+    "url": "/movie_app_2020/static/js/2.ddea00c0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie_app_2020/static/js/2.ddea00c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1639da50ef170a412dbd",
+    "url": "/movie_app_2020/static/js/main.b900a7b4.chunk.js"
+  },
+  {
+    "revision": "4839ee59ec104f2d85b3",
+    "url": "/movie_app_2020/static/js/runtime-main.2f3c173d.js"
+  }
+]);
